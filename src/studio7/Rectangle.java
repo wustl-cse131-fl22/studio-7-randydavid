8ab222a12871068e6a4a5b75b0cf66d248b94524
tracy <tracy@localhost>
2022-11-03 13:38:12 -0500

    push

--- src/studio7/Rectangle.java
+++ src/studio7/Rectangle.java
@@ -1,7 +1,41 @@
 package studio7;
 
 public class Rectangle {
-
+	
+	private int length;
+	private int width;
+	
+	public Rectangle(int length, int width) {
+		// TODO Auto-generated constructor stub
+		length = this.length;
+		width = this.length;
+	}
+	
+	
+	int width() {
+		return this.width;
+	}
+	
+	int length() {
+		return this.length;
+	}
+	
+	
+	int area() {
+		int res = this.length * this.width;
+		return res;
+	}
+	
+	boolean square() {
+		return this.length == this.width;
+	}
+	
+	boolean smaller(Rectangle another) {
+		return another.area() > this.area();
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
